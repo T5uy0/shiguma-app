2b70b6111236d572bf3ea3dd20a2b58add8e614a
Julien Schneider <julienschneider964@gmail.com>
2025-02-15 11:38:52 +0100

    Fix: Listen to Render PORT

--- config/puma.rb
+++ config/puma.rb
@@ -5,8 +5,7 @@ threads threads_count, threads_count
 preload_app!
 
 rackup DefaultRackup
-port ENV['PORT'] || 4567
+port  10000
 environment ENV['RACK_ENV'] || 'development'
-port ENV.fetch("PORT") { 4567 }
 bind 'tcp://0.0.0.0'
 environment ENV.fetch("RACK_ENV") { "development" }
