8a5071ebed37d1e6d66100967e494624672161c2
Julien Schneider <julienschneider964@gmail.com>
2025-02-15 11:45:52 +0100

    Fix Puma config for Render

--- config/puma.rb
+++ config/puma.rb
@@ -1,21 +1,25 @@
-# Nombre de workers (processus Puma)
-workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
+# Définition des workers (nombre de processus Puma)
+workers ENV.fetch('WEB_CONCURRENCY', 2).to_i
 
-# Nombre de threads par worker
-threads_count = Integer(ENV.fetch('MAX_THREADS', 5))
+# Définition des threads par worker
+threads_count = ENV.fetch('MAX_THREADS', 5).to_i
 threads threads_count, threads_count
 
-port  10000
+# Définition du port (Render assigne un port dynamique)
+port 10000
 
-# Écouter sur toutes les interfaces réseau
-bind 'tcp://0.0.0.0:' + ENV.fetch('PORT', '10000')
+# Lier le serveur à toutes les interfaces réseau
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 10000)}"
 
-# Définir l’environnement
+# Définition de l'environnement
 environment ENV.fetch('RACK_ENV', 'production')
 
-# Preload l’application pour de meilleures performances
+# Préchargement de l'application avant le fork (améliore les performances)
 preload_app!
 
-environment ENV['RACK_ENV'] || 'development'
-bind 'tcp://0.0.0.0'
-environment ENV.fetch("RACK_ENV") { "development" }
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+# Activer la journalisation si nécessaire
+stdout_redirect 'log/puma.stdout.log', 'log/puma.stderr.log', true if ENV['RACK_ENV'] == 'production'
