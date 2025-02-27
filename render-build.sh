e1b5590416ccb78e1272f2cf69b291b58aa7a6c1
Julien Schneider <julienschneider964@gmail.com>
2025-02-27 08:38:01 +0100

    feat:

--- render-build.sh
+++ render-build.sh
@@ -2,4 +2,17 @@
 # exit on error
 set -o errexit
 
+
+cat <<EOF > config/database.yml
+production:
+  adapter: mysql2
+  encoding: utf8mb4
+  reconnect: false
+  database: ${DATABASE_NAME}
+  username: ${DATABASE_USER}
+  password: ${DATABASE_PASSWORD}
+  host: ${DATABASE_HOST}
+  port: ${DATABASE_PORT:-3306}
+EOF
+
 bundle install
