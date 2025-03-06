f0d893929e1c6160cbf749844a19a3b37a5969ec
Julien Schneider <julienschneider964@gmail.com>
2025-03-06 10:16:54 +0100

    feat: remove line

--- render-build.sh
+++ render-build.sh
@@ -2,24 +2,24 @@
 # exit on error
 set -o errexit
 
-if [[ -z "$DATABASE_NAME" || -z "$DATABASE_USER" || -z "$DATABASE_PASSWORD" || -z "$DATABASE_HOST" ]]; then
-  echo "❌ ERREUR : Une ou plusieurs variables d'environnement ne sont pas définies."
-  exit 1
-fi
+# if [[ -z "$DATABASE_NAME" || -z "$DATABASE_USER" || -z "$DATABASE_PASSWORD" || -z "$DATABASE_HOST" ]]; then
+#   echo "❌ ERREUR : Une ou plusieurs variables d'environnement ne sont pas définies."
+#   exit 1
+# fi
 
-cat <<EOF > config/database.yml
-production:
-  adapter: mysql2
-  encoding: utf8mb4
-  reconnect: false
-  database: ${DATABASE_NAME}
-  username: ${DATABASE_USER}
-  password: ${DATABASE_PASSWORD}
-  host: ${DATABASE_HOST}
-  port: ${DATABASE_PORT:-22647}
-EOF
+# cat <<EOF > config/database.yml
+# production:
+#   adapter: mysql2
+#   encoding: utf8mb4
+#   reconnect: false
+#   database: ${DATABASE_NAME}
+#   username: ${DATABASE_USER}
+#   password: ${DATABASE_PASSWORD}
+#   host: ${DATABASE_HOST}
+#   port: ${DATABASE_PORT:-22647}
+# EOF
 
-echo "🛠️ Génération du fichier config/database.yml..."
+# echo "🛠️ Génération du fichier config/database.yml..."
 
 
 
